1b3e728cc0678718565f48c33417aae0caa0f130
Warren Harper II <warrenharper2@att.net>
2019-09-04 21:39:24 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,12 +7,14 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person
+    if person.class != Person # we need to tell the program to raise our PartnerError when the argument passed into the get_married is not an instance of the Person class.
       raise PartnerError
+    rescue PartnerError => error #The rescue method creates an instance of the PartnerError class and puts out the result of calling message on that instance.
+        puts error.message
     else
     person.partner = self
   end
- class PartnerError < StandardError
+ class PartnerError < StandardError #include the custom error class inside of our Person class.
  end
 end
 
